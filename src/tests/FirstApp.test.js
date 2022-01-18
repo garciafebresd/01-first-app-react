3cc8b033f94b8596f770b31db1e380aed69f8272
garciafebresd <garciafebresd@gmail.com>
2022-01-17 21:44:09 -0300

    first contact with enzyme

--- src/tests/FirstApp.test.js
+++ src/tests/FirstApp.test.js
@@ -1,18 +1,21 @@
 import React from 'react'
 import '@testing-library/jest-dom';
-import { render } from '@testing-library/react';
+import { shallow } from 'enzyme';
 import FirstApp from '../FirstApp';
 
 describe('Testing component <FirstApp />', () => {
 
+    // test('should show the message and subtitle', () => {
+    //     const greeting = 'Hola, Soy Goku';
+    //     const {getByText} = render(<FirstApp greeting= { greeting } />)
+    //     expect(getByText(greeting)).toBeInTheDocument();
+    // });
+
     test('should show the message and subtitle', () => {
-        
         const greeting = 'Hola, Soy Goku';
-        // const subtitle = 'Subtitle para prueba';
+        const wrapper = shallow(<FirstApp greeting= { greeting } />);
 
-        const {getByText} = render(<FirstApp greeting= { greeting } />)
-
-        expect(getByText(greeting)).toBeInTheDocument();
+        expect(wrapper).toMatchSnapshot();
     });
     
 });
